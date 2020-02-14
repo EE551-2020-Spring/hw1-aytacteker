ca8b65009190939764b5161aea4fab26bed8cb1e
aytacteker <60752062+aytacteker@users.noreply.github.com>
2020-02-14 01:44:19 -0500

    Update EE551_HW1.py

--- EE551_HW1.py
+++ EE551_HW1.py
@@ -24,6 +24,7 @@ def numbers_and_strings():
     # Replace "good" with "awesome" in variable m and assign it to a new variable n
     n = m.replace("good", "awesome")
     return x, y, z, length, m, n
+
 def lists():
     """
     This is to review basic operations with lists.
@@ -38,19 +39,17 @@ def lists():
     #   first row is [1, 4, 5]
     #   second row is [6, 10, 11]
     #   third row is [12, 17, 38]
+    import numpy as np
     c = np.array([[1,4,5], [6,10,11], [12,17,38]])
     # Collect the items in the last column of matrix A using list comprehension
-    d = c[:,2]
+    print(c[:,2])
     # Collect only the even items of the diagonal of matrix A using list comprehension
-    o = [x for x in c.diagonal() if x % 2 == 0] 
+    d = [x for x in c.diagonal() if x % 2 == 0] 
     # We can convert a single character to its underlying integer code (e.g., its ASCII byte value)
     # by passing it to the built-in ord function. Generate a list of these integers to represent
     # each character of the string "Stevens" using list comprehension.
-    a = [ord(x) for x in (p[0])]
-
-    
-    return p, r, c, d, o, a
-
+    o = [ord(x) for x in (p[0])]
+    return p, r, c, d, o
 
 def dictionaries():
     """
@@ -60,27 +59,27 @@ def dictionaries():
     #   fruit => "apple"
     #   quantity => 4
     #   color => "green"
-    a = {"fruit":"apple", "quantity":"4", "color":"green"}
+    f = {"fruit":"apple", "quantity":"4", "color":"green"}
     # Get the item in dictionary f that the key "fruit" maps to
-    f = a["fruit"]
+    a = f["fruit"]
     # Increase the quantity of f by 1
     # IMPLEMENT IT HERE
-    a["quantity"]=float(a["quantity"])+1
+    f["quantity"] = float(f["quantity"]) + 1
     # Create a nested dictionary where:
     #   name => {first_name => "Grace", last_name => "Hopper"} (a dictionary)
     #   jobs => ["scientist", "engineer"] (a list)
     #   age => 85
-   
-    p = {'name': {"first_name":"Grace", "last_name":"Hopper"},
-                'jobs': "['scientist', 'engineer']",
-                  'age':'85'}
+    amazing_grace = {'name': {'first_name':'Grace', 'last_name':'Hopper'},
+                'jobs': ['scientist', 'engineer'],
+                'age': 85}
     # Add "programmer" to the list of jobs Grace has
     # IMPLEMENT IT HERE
-    
+    amazing_grace['jobs'].append('programmer')      
     # Get the third job Grace has that you recently added
-
+    p = amazing_grace['jobs'][2]
     # Use the sort() function to get sorted keys of amazing_grace in alphabetically ascending order
-
+    k = list(amazing_grace.keys())
+    k.sort()
     return a, f, p, k
 
 numbers_and_strings()
